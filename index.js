c2773e5fdb96e8d277a1cd9fedb7081b413869dd
Jolene <74627192+JoleneMel@users.noreply.github.com>
2021-03-03 19:57:40 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-console.log($('body'));
-
 let square = $('.square');
-console.log(square);
-
 let ticGird = $('#tic.Grid');
 const squares = Array.from(document.getElementsByClassName("square"));
-//have ids for each of them
-console.log(squares.id);
 let playerO = 'O';
 let playerX = 'X';
 let ticroundtxt = document.getElementById('ticroundtxt');
-
+let winnerBanner = $('#winnerIsBanner');
 //to start w/O
 let currentPlayer = playerO;
-
 const startGame = () => {
 
 //to make square clickable
@@ -22,74 +15,113 @@ const startGame = () => {
     });
 
     function squareClicked(e) {
-        //target = squares id
-        const id = e.target.id;
-        // if(!squares[id]) {
+        //currenttarget = squares id
+        const id = e.currentTarget;
         //to change the square to currentPlayer
         squares[id] = currentPlayer;
-        e.target.innerHTML = currentPlayer;
+        e.currentTarget.innerHTML = currentPlayer;
         //if player has won then the game will cease 
-        if(winOccurs(currentPlayer)) {
-            //txt is cleared for turn order
-            ticroundtxt.innerHTML = '';
+        if (winOccurs(currentPlayer)) {
+        //     //txt is cleared for turn order
+            ticroundtxt.innerHTML = `${currentPlayer} won`;
             return;
         }
         //to rotate the player order
         currentPlayer = currentPlayer === playerO ? playerX : playerO;
         //to reflect the turn order 
         ticroundtxt.innerHTML = `${currentPlayer}'s turn`;
-    // }else if(!winOccurs(currentPlayer) && )
+
     }
 }
+
 
 const winOccurs = (currentPlayer) => {
-    if (squares[0] === currentPlayer) {
-      (squares[1] && squares[2] === currentPlayer); {
-        $('#winnerIsBanner').removeClass('hideMe');
-        winnerIsBanner.innerHTML = `${currentPlayer} has won the top row!!!`
+    
+    console.log(squares);
+    if ($('.square.r1.c1').text() === playerO && $('.square.r1.c2').text() === playerO && $('.square.r1.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the top row!!!`);
         return true;
     }
-}
-}
+    if ($('.square.r1.c1').text() === playerX && $('.square.r1.c2').text() === playerX && $('.square.r1.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the top row!!!`);
+        return true;
+    }
+    //okay above one works.
+    else if ($('.square.r2.c1').text() === playerO && $('.square.r2.c2').text() === playerO && $('.square.r2.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the middle row!!!`);
+        return true;
+    }
+    else if ($('.square.r2.c1').text() === playerX && $('.square.r2.c2').text() === playerX && $('.square.r2.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the middle row!!!`);
+        return true;
+    }
+    //above one works
+    else if ($('.square.r3.c1').text() === playerO && $('.square.r3.c2').text() === playerO && $('.square.r3.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the bottom row!!!`);
+        return true;
+    }
+    else if ($('.square.r3.c1').text() === playerX && $('.square.r3.c2').text() === playerX && $('.square.r3.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the bottom row!!!`);
+        return true;
+    }
+    //the above 3 work together.
+    else if ($('.square.r1.c1').text() === playerO && $('.square.r2.c1').text() === playerO && $('.square.r3.c1').text() === playerO) {
+        console.log(`${currentPlayer} has won the left row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c1').text() === playerX && $('.square.r2.c1').text() === playerX && $('.square.r3.c1').text() === playerX) {
+        console.log(`${currentPlayer} has won the left row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c2').text() === playerO&& $('.square.r2.c2').text() === playerO && $('.square.r3.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the vertical row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c2').text() === playerX && $('.square.r2.c2').text() === playerX && $('.square.r3.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the vertical row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c3').text() === playerO && $('.square.r2.c3').text() === playerO && $('.square.r3.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the vertical left row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c3').text() === playerX && $('.square.r2.c3').text() === playerX && $('.square.r3.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the vertical left row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c1').text() === playerO && $('.square.r2.c2').text() === playerO && $('.square.r3.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the left diagonal row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c1').text() === playerX && $('.square.r2.c2').text() === playerX && $('.square.r3.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the left diagonal row!!!`);
+        return true;
+    }
+    else if ($('.square.r1.c3').text() === playerO && $('.square.r2.c3').text() === playerO && $('.square.r3.c3').text() === playerO) {
+        console.log(`${currentPlayer} has won the right row!!!`);
+        return true;
+    } 
+    else if ($('.square.r1.c3').text() === playerX && $('.square.r2.c3').text() === playerX && $('.square.r3.c3').text() === playerX) {
+        console.log(`${currentPlayer} has won the right row!!!`);
+        return true;
+    } 
+    else if ($('.square.r1.c3').text() === playerX && $('.square.r2.c2').text() === playerX && $('.square.r3.c1').text() === playerX) {
+        console.log(`${currentPlayer} has won`);
+        return true;
+    } 
+    else if ($('.square.r1.c3').text() === playerO && $('.square.r2.c2').text() === playerO && $('.square.r3.c1').text() === playerO) {
+        console.log(`${currentPlayer} has won`);
+        return true;
+    }
+};
 
-
-//     if (squares[3] && squares[4] && squares[5] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('hideMe');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the middle row!!!`
-//         return true;
-//     }
-//     if (squares[6] && squares[7] && squares[8] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('hideMe');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the bottom row!!!`
-//         return true;
-//     }
-//     if (squares[0] && squares[3] && squares[6] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('hideMe');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the left row!!!`
-//         return true;
-//     }
-//     if (squares[1] && squares[4] && squares[7] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('hideMe');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the vertical row!!!`
-//         return true;
-//     }
-//     if (squares[2] && squares[5] && squares[8] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('d-none');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the vertical left row!!!`
-//         return true;
-//     }
-//     if (squares[0] && squares[4] && squares[8] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('hideMe');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the left diagonal row!!!`
-//         return true;
-//     }
-//     if (squares[2] && squares[5] && squares[8] === currentPlayer) {
-//         $('#winnerIsBanner').removeClass('hideMe');
-//         winnerIsBanner.innerHTML = `${currentPlayer} has won the right row!!!`
-//         return true;
-//     } 
-// };
-
-// }
-
+restart.addEventListener("click", () => {
+    squares.forEach((square) => {
+      square.innerText = "";
+    });
+    currentPlayer = playerO;
+});
 startGame ();
+
+
+
